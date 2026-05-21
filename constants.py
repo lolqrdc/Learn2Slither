01db7fd1d63072fcdff3c85a45ce24405df50bca
lola <lola-rdc@outlook.fr>
2026-05-21 11:43:54 +0200

    fix: handling flake8 norm error

--- constants.py
+++ constants.py
@@ -5,17 +5,20 @@ INIT_SNAKE_LENGTH = 3
 NUM_GREEN_APPLES = 2
 NUM_RED_APPLES = 1
 
+
 class Action(Enum):
     UP = 1
     DOWN = 2
     LEFT = 3
     RIGHT = 4
 
+
 class Event(Enum):
     GREEN = "green"
     RED = "red"
     DEAD = "dead"
     NONE = "none"
+
 
 REWARD_GREEN_APPLE = 10
 REWARD_RED_APPLE = -10
